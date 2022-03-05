cae4f0ea143134d2e1145ff9202f53c3dd93a931
isailara <isailara20002@gmail.com>
2022-03-05 17:08:00 -0600

    Herencia

--- src/uaslp/objetos/parcial2/Cliente.java
+++ src/uaslp/objetos/parcial2/Cliente.java
@@ -1,4 +1,13 @@
 package uaslp.objetos.parcial2;
 
 public class Cliente extends uaslp.objetos.parcial2.Persona{
+    private String telefonoDeContacto;
+
+    public String getTelefonoDeContacto() {
+        return telefonoDeContacto;
+    }
+
+    public void setTelefonoDeContacto(String telefonoDeContacto) {
+        this.telefonoDeContacto = telefonoDeContacto;
+    }
 }
